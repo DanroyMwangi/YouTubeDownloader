94f96186dc1d3a6aa187a7dd7d7363b9e3edc95d
DanroyNdungu <danroy.mwangi@strathmore.edu>
2022-07-11 16:36:52 +0300

    Audio only download bug fixes.

--- main.py
+++ main.py
@@ -4,25 +4,26 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-global videoSize
+global fileSize
 pytube.request.default_range_size = 9437184
 
 
 def progress(stream, chunk, remaining):
-    percent = ((videoSize-remaining)*100)/videoSize
+    percent = ((fileSize-remaining)*100)/fileSize
     print("Completed:%0.00f%s" % (percent, '%'))
 
 
 link = input('Enter the link to the video you want:')
+print('Link is being processed...')
 youtubeVideo = YouTube(link, on_progress_callback=progress)
 hdStream = youtubeVideo.streams
-fileType = int(input('Do you want:\n1. Audio only\n2. Video only\n3. Both video and audio'))
+fileType = int(input('Do you want:\n1. Audio only\n2. Video only\n3. Both video and audio\n'))
 
 match fileType:
     case 1:
-        audio = hdStream.filter(only_audio=True, progressive=True).get_audio_only()
-        audioSize = audio.filesize
-        fileName = audio.title + ' audio' + audio.subtype
+        audio = hdStream.filter(only_audio=True).get_audio_only()
+        fileSize = audio.filesize
+        fileName = audio.title + ' audio.' + audio.subtype
         print('%s has began downloading...' % fileName)
         audio.download(os.getenv('DOWNLOAD_PATH'), fileName)
         print('Download complete.')
@@ -32,42 +33,42 @@ match fileType:
         match resolution:
             case 1:
                 video = hdStream.filter(only_video=True, resolution='144p', progressive=True).first()
-                videoSize = video.filesize
+                fileSize = video.filesize
                 fileName = video.title + ' 144p.' + video.subtype
                 print('%s has began downloading...' % fileName)
                 video.download(os.getenv('DOWNLOAD_PATH'), fileName)
                 print('Download complete.')
             case 2:
                 video = hdStream.filter(only_video=True,resolution='360p', progressive=True).first()
-                videoSize = video.filesize
+                fileSize = video.filesize
                 fileName = video.title + ' 360p.' + video.subtype
                 print('%s has began downloading...' % fileName)
                 video.download(os.getenv('DOWNLOAD_PATH'), fileName)
                 print('Download complete.')
             case 3:
                 video = hdStream.filter(only_video=True,resolution='720p', progressive=True).first()
-                videoSize = video.filesize
+                fileSize = video.filesize
                 fileName = video.title + ' 720p.' + video.subtype
                 print('%s has began downloading...' % fileName)
                 video.download(os.getenv('DOWNLOAD_PATH'), fileName)
                 print('Download complete.')
             case 4:
                 video = hdStream.filter(only_video=True,resolution='1080p', progressive=True).first()
-                videoSize = video.filesize
+                fileSize = video.filesize
                 fileName = video.title + ' 1080p.' + video.subtype
                 print('%s has began downloading...' % fileName)
                 video.download(os.getenv('DOWNLOAD_PATH'), fileName)
                 print('Download complete.')
             case 5:
                 video = hdStream.filter(only_video=True,progressive=True).get_highest_resolution()
-                videoSize = video.filesize
+                fileSize = video.filesize
                 fileName = video.title + ' HD.' + video.subtype
                 print('%s has began downloading...' % fileName)
                 video.download(os.getenv('DOWNLOAD_PATH'), fileName)
                 print('Download complete.')
             case 6:
                 video = hdStream.filter(only_video=True,progressive=True).get_lowest_resolution()
-                videoSize = video.filesize
+                fileSize = video.filesize
                 fileName = video.title + ' Lowest.' + video.subtype
                 print('%s has began downloading...' % fileName)
                 video.download(os.getenv('DOWNLOAD_PATH'), fileName)
@@ -78,42 +79,42 @@ match fileType:
         match resolution:
             case 1:
                 video = hdStream.filter(resolution='144p', progressive=True).first()
-                videoSize = video.filesize
+                fileSize = video.filesize
                 fileName = video.title + ' 144p.' + video.subtype
                 print('%s has began downloading...' % fileName)
                 video.download(os.getenv('DOWNLOAD_PATH'), fileName)
                 print('Download complete.')
             case 2:
                 video = hdStream.filter(resolution='360p', progressive=True).first()
-                videoSize = video.filesize
+                fileSize = video.filesize
                 fileName = video.title + ' 360p.' + video.subtype
                 print('%s has began downloading...' % fileName)
                 video.download(os.getenv('DOWNLOAD_PATH'), fileName)
                 print('Download complete.')
             case 3:
                 video = hdStream.filter(resolution='720p', progressive=True).first()
-                videoSize = video.filesize
+                fileSize = video.filesize
                 fileName = video.title + ' 720p.' + video.subtype
                 print('%s has began downloading...' % fileName)
                 video.download(os.getenv('DOWNLOAD_PATH'), fileName)
                 print('Download complete.')
             case 4:
                 video = hdStream.filter(resolution='1080p', progressive=True).first()
-                videoSize = video.filesize
+                fileSize = video.filesize
                 fileName = video.title + ' 1080p.' + video.subtype
                 print('%s has began downloading...' % fileName)
                 video.download(os.getenv('DOWNLOAD_PATH'), fileName)
                 print('Download complete.')
             case 5:
                 video = hdStream.filter(progressive=True).get_highest_resolution()
-                videoSize = video.filesize
+                fileSize = video.filesize
                 fileName = video.title + ' HD.' + video.subtype
                 print('%s has began downloading...' % fileName)
                 video.download(os.getenv('DOWNLOAD_PATH'), fileName)
                 print('Download complete.')
             case 6:
                 video = hdStream.filter(progressive=True).get_lowest_resolution()
-                videoSize = video.filesize
+                fileSize = video.filesize
                 fileName = video.title + ' Lowest.' + video.subtype
                 print('%s has began downloading...' % fileName)
                 video.download(os.getenv('DOWNLOAD_PATH'), fileName)
