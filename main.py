61bba740e1f4943add42f75db1873179c240e14d
DanroyNdungu <danroy.mwangi@strathmore.edu>
2022-07-11 16:49:39 +0300

    Exception and error handling has been added to cater for the errors encountered during the previous iterations of the application. The AttributeError and RegexMatchError have been handled with custom error messages. All other errors have been handled and the error message displays the type of error encountered.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pytube
 from pytube import YouTube
 import os
 from dotenv import load_dotenv
+from pytube.exceptions import RegexMatchError
+
 load_dotenv()
 
 global fileSize
@@ -15,107 +17,190 @@ def progress(stream, chunk, remaining):
 
 link = input('Enter the link to the video you want:')
 print('Link is being processed...')
-youtubeVideo = YouTube(link, on_progress_callback=progress)
-hdStream = youtubeVideo.streams
-fileType = int(input('Do you want:\n1. Audio only\n2. Video only\n3. Both video and audio\n'))
+try:
+    youtubeVideo = YouTube(link, on_progress_callback=progress)
+except RegexMatchError as e:
+    print('Please recheck the link you entered.')
+else:
+    hdStream = youtubeVideo.streams
+    fileType = int(input('Do you want:\n1. Audio only\n2. Video only\n3. Both video and audio\n'))
 
-match fileType:
-    case 1:
-        audio = hdStream.filter(only_audio=True).get_audio_only()
-        fileSize = audio.filesize
-        fileName = audio.title + ' audio.' + audio.subtype
-        print('%s has began downloading...' % fileName)
-        audio.download(os.getenv('DOWNLOAD_PATH'), fileName)
-        print('Download complete.')
-    case 2:
-        resolution = int(input('Choose the quality you want.\n1. 144p\n2. 360p\n3. 720p\n4. 1080p\n5. Best quality\n6. Lowest quality\n'))
-        # https://www.youtube.com/watch?v=9txjIvXafvc
-        match resolution:
-            case 1:
-                video = hdStream.filter(only_video=True, resolution='144p', progressive=True).first()
-                fileSize = video.filesize
-                fileName = video.title + ' 144p.' + video.subtype
+    match fileType:
+        case 1:
+            try:
+                audio = hdStream.filter(only_audio=True).get_audio_only()
+                fileSize = audio.filesize
+                fileName = audio.title + ' audio.' + audio.subtype
                 print('%s has began downloading...' % fileName)
-                video.download(os.getenv('DOWNLOAD_PATH'), fileName)
+                audio.download(os.getenv('DOWNLOAD_PATH'), fileName)
                 print('Download complete.')
-            case 2:
-                video = hdStream.filter(only_video=True,resolution='360p', progressive=True).first()
-                fileSize = video.filesize
-                fileName = video.title + ' 360p.' + video.subtype
-                print('%s has began downloading...' % fileName)
-                video.download(os.getenv('DOWNLOAD_PATH'), fileName)
+            except AttributeError as e:
+                print('This quality is not available')
+            except Exception as e:
+                print(e)
+            else:
                 print('Download complete.')
-            case 3:
-                video = hdStream.filter(only_video=True,resolution='720p', progressive=True).first()
-                fileSize = video.filesize
-                fileName = video.title + ' 720p.' + video.subtype
-                print('%s has began downloading...' % fileName)
-                video.download(os.getenv('DOWNLOAD_PATH'), fileName)
-                print('Download complete.')
-            case 4:
-                video = hdStream.filter(only_video=True,resolution='1080p', progressive=True).first()
-                fileSize = video.filesize
-                fileName = video.title + ' 1080p.' + video.subtype
-                print('%s has began downloading...' % fileName)
-                video.download(os.getenv('DOWNLOAD_PATH'), fileName)
-                print('Download complete.')
-            case 5:
-                video = hdStream.filter(only_video=True,progressive=True).get_highest_resolution()
-                fileSize = video.filesize
-                fileName = video.title + ' HD.' + video.subtype
-                print('%s has began downloading...' % fileName)
-                video.download(os.getenv('DOWNLOAD_PATH'), fileName)
-                print('Download complete.')
-            case 6:
-                video = hdStream.filter(only_video=True,progressive=True).get_lowest_resolution()
-                fileSize = video.filesize
-                fileName = video.title + ' Lowest.' + video.subtype
-                print('%s has began downloading...' % fileName)
-                video.download(os.getenv('DOWNLOAD_PATH'), fileName)
-                print('Download complete.')
-    case 3:
-        resolution = int(input('Choose the quality you want.\n1. 144p\n2. 360p\n3. 720p\n4. 1080p\n5. Best quality\n6. Lowest quality\n'))
-        # https://www.youtube.com/watch?v=9txjIvXafvc
-        match resolution:
-            case 1:
-                video = hdStream.filter(resolution='144p', progressive=True).first()
-                fileSize = video.filesize
-                fileName = video.title + ' 144p.' + video.subtype
-                print('%s has began downloading...' % fileName)
-                video.download(os.getenv('DOWNLOAD_PATH'), fileName)
-                print('Download complete.')
-            case 2:
-                video = hdStream.filter(resolution='360p', progressive=True).first()
-                fileSize = video.filesize
-                fileName = video.title + ' 360p.' + video.subtype
-                print('%s has began downloading...' % fileName)
-                video.download(os.getenv('DOWNLOAD_PATH'), fileName)
-                print('Download complete.')
-            case 3:
-                video = hdStream.filter(resolution='720p', progressive=True).first()
-                fileSize = video.filesize
-                fileName = video.title + ' 720p.' + video.subtype
-                print('%s has began downloading...' % fileName)
-                video.download(os.getenv('DOWNLOAD_PATH'), fileName)
-                print('Download complete.')
-            case 4:
-                video = hdStream.filter(resolution='1080p', progressive=True).first()
-                fileSize = video.filesize
-                fileName = video.title + ' 1080p.' + video.subtype
-                print('%s has began downloading...' % fileName)
-                video.download(os.getenv('DOWNLOAD_PATH'), fileName)
-                print('Download complete.')
-            case 5:
-                video = hdStream.filter(progressive=True).get_highest_resolution()
-                fileSize = video.filesize
-                fileName = video.title + ' HD.' + video.subtype
-                print('%s has began downloading...' % fileName)
-                video.download(os.getenv('DOWNLOAD_PATH'), fileName)
-                print('Download complete.')
-            case 6:
-                video = hdStream.filter(progressive=True).get_lowest_resolution()
-                fileSize = video.filesize
-                fileName = video.title + ' Lowest.' + video.subtype
-                print('%s has began downloading...' % fileName)
-                video.download(os.getenv('DOWNLOAD_PATH'), fileName)
-                print('Download complete.')
+        case 2:
+            resolution = int(input('Choose the quality you want.\n1. 144p\n2. 360p\n3. 720p\n4. 1080p\n5. Best quality\n6. Lowest quality\n'))
+            # https://www.youtube.com/watch?v=9txjIvXafvc
+            match resolution:
+                case 1:
+                    try:
+                        video = hdStream.filter(only_video=True, resolution='144p', progressive=True).first()
+                        fileSize = video.filesize
+                        fileName = video.title + ' 144p.' + video.subtype
+                        print('%s has began downloading...' % fileName)
+                        video.download(os.getenv('DOWNLOAD_PATH'), fileName)
+                    except AttributeError as e:
+                        print('This quality is not available')
+                    except Exception as e:
+                        print(e)
+                    else:
+                        print('Download complete.')
+                case 2:
+                    try:
+                        video = hdStream.filter(only_video=True, resolution='360p', progressive=True).first()
+                        fileSize = video.filesize
+                        fileName = video.title + ' 360p.' + video.subtype
+                        print('%s has began downloading...' % fileName)
+                        video.download(os.getenv('DOWNLOAD_PATH'), fileName)
+                    except AttributeError as e:
+                        print('This quality is not available')
+                    except Exception as e:
+                        print(e)
+                    else:
+                        print('Download complete.')
+                case 3:
+                    try:
+                        video = hdStream.filter(only_video=True, resolution='720p', progressive=True).first()
+                        fileSize = video.filesize
+                        fileName = video.title + ' 720p.' + video.subtype
+                        print('%s has began downloading...' % fileName)
+                        video.download(os.getenv('DOWNLOAD_PATH'), fileName)
+                    except AttributeError as e:
+                        print('This quality is not available')
+                    except Exception as e:
+                        print(e)
+                    else:
+                        print('Download complete.')
+                case 4:
+                    try:
+                        video = hdStream.filter(only_video=True, resolution='1080p', progressive=True).first()
+                        fileSize = video.filesize
+                        fileName = video.title + ' 1080p.' + video.subtype
+                        print('%s has began downloading...' % fileName)
+                        video.download(os.getenv('DOWNLOAD_PATH'), fileName)
+                    except AttributeError as e:
+                        print('This quality is not available')
+                    except Exception as e:
+                        print(e)
+                    else:
+                        print('Download complete.')
+                case 5:
+                    try:
+                        video = hdStream.filter(only_video=True, progressive=True).get_highest_resolution()
+                        fileSize = video.filesize
+                        fileName = video.title + ' HD.' + video.subtype
+                        print('%s has began downloading...' % fileName)
+                        video.download(os.getenv('DOWNLOAD_PATH'), fileName)
+                    except AttributeError as e:
+                        print('This quality is not available')
+                    except Exception as e:
+                        print(e)
+                    else:
+                        print('Download complete.')
+                case 6:
+                    try:
+                        video = hdStream.filter(only_video=True, progressive=True).get_lowest_resolution()
+                        fileSize = video.filesize
+                        fileName = video.title + ' Lowest.' + video.subtype
+                        print('%s has began downloading...' % fileName)
+                        video.download(os.getenv('DOWNLOAD_PATH'), fileName)
+                    except AttributeError as e:
+                        print('This quality is not available')
+                    except Exception as e:
+                        print(e)
+                    else:
+                        print('Download complete.')
+        case 3:
+            resolution = int(input('Choose the quality you want.\n1. 144p\n2. 360p\n3. 720p\n4. 1080p\n5. Best quality\n6. Lowest quality\n'))
+            # https://www.youtube.com/watch?v=9txjIvXafvc
+            match resolution:
+                case 1:
+                    try:
+                        video = hdStream.filter(resolution='144p', progressive=True).first()
+                        fileSize = video.filesize
+                        fileName = video.title + ' 144p.' + video.subtype
+                        print('%s has began downloading...' % fileName)
+                        video.download(os.getenv('DOWNLOAD_PATH'), fileName)
+                    except AttributeError as e:
+                        print('This quality is not available')
+                    except Exception as e:
+                        print(e)
+                    else:
+                        print('Download complete.')
+                case 2:
+                    try:
+                        video = hdStream.filter(resolution='360p', progressive=True).first()
+                        fileSize = video.filesize
+                        fileName = video.title + ' 360p.' + video.subtype
+                        print('%s has began downloading...' % fileName)
+                        video.download(os.getenv('DOWNLOAD_PATH'), fileName)
+                    except AttributeError as e:
+                        print('This quality is not available')
+                    except Exception as e:
+                        print(e)
+                    else:
+                        print('Download complete.')
+                case 3:
+                    try:
+                        video = hdStream.filter(resolution='720p', progressive=True).first()
+                        fileSize = video.filesize
+                        fileName = video.title + ' 720p.' + video.subtype
+                        print('%s has began downloading...' % fileName)
+                        video.download(os.getenv('DOWNLOAD_PATH'), fileName)
+                    except AttributeError as e:
+                        print('This quality is not available')
+                    except Exception as e:
+                        print(e)
+                    else:
+                        print('Download complete.')
+                case 4:
+                    try:
+                        video = hdStream.filter(resolution='1080p', progressive=True).first()
+                        fileSize = video.filesize
+                        fileName = video.title + ' 1080p.' + video.subtype
+                        print('%s has began downloading...' % fileName)
+                        video.download(os.getenv('DOWNLOAD_PATH'), fileName)
+                    except AttributeError as e:
+                        print('This quality is not available')
+                    except Exception as e:
+                        print(e)
+                    else:
+                        print('Download complete.')
+                case 5:
+                    try:
+                        video = hdStream.filter(progressive=True).get_highest_resolution()
+                        fileSize = video.filesize
+                        fileName = video.title + ' HD.' + video.subtype
+                        print('%s has began downloading...' % fileName)
+                        video.download(os.getenv('DOWNLOAD_PATH'), fileName)
+                    except AttributeError as e:
+                        print('This quality is not available')
+                    except Exception as e:
+                        print(e)
+                    else:
+                        print('Download complete.')
+                case 6:
+                    try:
+                        video = hdStream.filter(progressive=True).get_lowest_resolution()
+                        fileSize = video.filesize
+                        fileName = video.title + ' Lowest.' + video.subtype
+                        print('%s has began downloading...' % fileName)
+                        video.download(os.getenv('DOWNLOAD_PATH'), fileName)
+                    except AttributeError as e:
+                        print('This quality is not available')
+                    except Exception as e:
+                        print(e)
+                    else:
+                        print('Download complete.')
